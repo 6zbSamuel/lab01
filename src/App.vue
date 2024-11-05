3a036d3d0256d3e76ea903dd2d32d6b91a2bff6b
6zbSamuel <lzb2583842928@outlook.com>
2024-11-05 23:11:36 +0800

    6

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,41 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import { useMessageStore } from '@/stores/message'
+import { storeToRefs } from 'pinia'
+import PageSizeSelector from './components/PageSizeSelector.vue'
 
+const store = useMessageStore()
+const { message } =storeToRefs(store)
 </script>
 
 <template>
-<div id="layout">
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Event</RouterLink> |
-        <RouterLink to="/about">About</RouterLink> |
-        <RouterLink to="/students">Student</RouterLink> 
-
-      </nav>
-    </div>
-  </header>
-  <RouterView />
-</div>
-
-
+  <div id="layout">
+    <header>
+      <div id="flashMessage" v-if="message">
+        <h4>{{ message }}</h4>
+      </div>
+      <div class="wrapper">
+        <nav>
+          <RouterLink :to="{ name: 'event-list-view' }">Event</RouterLink> |
+          <RouterLink :to="{ name: 'about' }">About</RouterLink> |
+          <RouterLink :to="{ name: 'students' }">Student</RouterLink> 
+          <PageSizeSelector />
+        </nav>
+      </div>
+    </header>
+    <RouterView />
+  </div>
 </template>
 
 <style>
-#layout{
+#layout {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
-nav{
+nav {
   padding: 30px;
 }
 nav a {
@@ -42,5 +48,17 @@ nav a.router-link-exact-active {
 
 h2 {
   font-size: 20px;
+}
+
+@keyframes yellofade {
+  from {
+    background-color: yellow;
+  }
+  to {
+    background-color: transparent;
+  }
+}
+#flashMessage {
+  animation: yellofade 3s ease-in-out;
 }
 </style>
